e797c829f06d9229f41e3d973256aee2f0cc715b
Krishna-B5 <krishnab.05@gmail.com>
2022-08-20 13:31:11 +0530

    Output Formatting

--- UdemyCourse/src/main/scala/pack/obj.scala
+++ UdemyCourse/src/main/scala/pack/obj.scala
@@ -2,9 +2,21 @@ package pack
 
 object obj {
   
-  def main(args: Array[String]){
+  def main(args: Array[String]): Unit={
     
-    println("Udemy Course")
+    println("Udemy Courses")
+    
+    val num: Int = 5
+    val name = "Krishna"
+    
+    println()
+    println("My name and lucky number "+name +num)
+    
+    println(s"My lucky number is $num");
+    println()
+    println(s"My name is $name")
+    
+    println(f"Number is $num%05d")
     
   }
 }

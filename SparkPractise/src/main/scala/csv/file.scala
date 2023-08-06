57c6b10cff11b8dd327ca9a035673ed4ea4d3be1
Krishna-B5 <krishnab.05@gmail.com>
2023-08-06 16:47:47 +0530

    BASICCSV

--- SparkPractise/src/main/scala/csv/file.scala
+++ SparkPractise/src/main/scala/csv/file.scala
@@ -1,9 +1,27 @@
 package csv
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+
 object file {
-  
-  def main(args: Array[String]){
-    
-    println("Reading a CSV file")
-  }
+
+	def main(args: Array[String]):Unit={
+
+			println("Reading a CSV file")
+
+			val conf = new SparkConf().setAppName("Master").setMaster("local[*]")
+			val sc = new SparkContext(conf)
+			sc.setLogLevel("Error")
+
+			val spark = SparkSession.builder().getOrCreate()
+			import spark.implicits._
+
+			val df = spark.read.format("csv")
+			         .option(header, true)
+			         .load("C:/data/CSV/addresses.csv")
+			df.show()
+
+	}
 }
